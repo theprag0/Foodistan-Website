f0d9a5acbc31d93f243a1a4d60da75aa3da17b8a
Harkirat Singh <harkiratsinghrocks1@gmail.com>
2021-09-26 19:57:30 +0530

    header

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import '../src/components/css/style.css'
-import App from './App';
+import '../src/components/css/style.css';
+import '../src/components/css/header.css';
+import '../src/components/css/SpecialOffer.css';
+import '../src/components/css/DealsOfTheDay.css';
+import { Route,BrowserRouter as Router,Switch } from "react-router-dom";
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
