f2521bcb38f1d6aab3af87df8e7b7a90db045ab9
Harkirat Singh <harkiratsinghrocks1@gmail.com>
2022-01-31 12:30:15 +0530

    razorpay

--- src/components/RestaurantsDetail/Menu/Cart/CartCard/CartCard.js
+++ src/components/RestaurantsDetail/Menu/Cart/CartCard/CartCard.js
@@ -4,8 +4,64 @@ import { HiShoppingCart } from 'react-icons/hi';
 import CartLink from '../CartLink/CartLink';
 import './CartCard.css';
 
+function loadScript(src){
+  return new Promise(resolve => {
+  const script = document.createElement('script');
+  script.src = src;
+  script.onload = () => {
+    resolve(true)
+  };
+  script.onerror = () =>{
+    resolve(false)
+  }
+  document.body.appendChild(script);
+});
+}
+
 const CartCard = (props) => {
   const cartRef = useRef();
+
+  // Razor pay settings 
+ async function displayRazorpay(){
+    const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+    if(!res){
+      alert('Check your connection!')
+      return
+    }
+
+    const data = await fetch('http://localhost:5000/razorpay', {method: 'POST'}).then((t) =>t.json())
+    console.log(data);
+
+    var options = {
+      key: "rzp_test_3kKNGSEXEeVCyd", // Enter the Key ID generated from the Dashboard
+      amount: data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: data.currency,
+      name: "Foodistaan",
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: data.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: function (response){
+          alert(response.razorpay_payment_id);
+          alert(response.razorpay_order_id);
+          alert(response.razorpay_signature)
+      },
+      prefill: {
+          "name": "Gaurav Kumar",
+          "email": "gaurav.kumar@example.com",
+          "contact": "9999999999"
+      },
+      notes: {
+          "address": "Razorpay Corporate Office"
+      },
+      "theme": {
+          "color": "#3399cc"
+      }
+  };
+  const paymentOject = new window.Razorpay(options);
+  paymentOject.open();
+  }
+  
+
 
   return (
     <div className={props.checkout ? 'checkout' : 'cart'} ref={cartRef}>
@@ -30,7 +86,7 @@ const CartCard = (props) => {
           {!props.checkout && <CartLink />}
           {props.checkout && (
             <div className="checkout-actions">
-              <button className="checkout-cart__btn">Order Now</button>
+              <button onClick={displayRazorpay} className="checkout-cart__btn">Order Now</button>
             </div>
           )}
         </div>
