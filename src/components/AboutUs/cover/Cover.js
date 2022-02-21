eed265bf6aaa8c0752705e4028e612ad0d2c6b6f
Harkirat Singh <harkiratsinghrocks1@gmail.com>
2022-02-21 20:06:12 +0530

    about us css done

--- src/components/AboutUs/cover/Cover.js
+++ src/components/AboutUs/cover/Cover.js
@@ -8,14 +8,14 @@ const Cover = () => {
   return (
     <div className='cover'>
       <div className='row'>
-        <div className='col-4'>
+        <div className='col-4 search'>
           <h1 className='cover-title'>Love <span className='cover-title2'>Street <br />Food?</span></h1>
           <h1 className='cover-sub-title'>Enjoy your favorite street food from the comfort and safety of your home.</h1>
           <button className='cover-orderNow'>Order Now</button>
         </div>
         <div className='col-8 container-fluid'>
-          <div className='circle-img-wrapper'>
-            <img src={circle} className='circle-img'></img>
+          <div className='circle-img-wrapper '>
+            <img src={circle} className='circle-img '></img>
             <img src={rectangle6} className='rectangle-left'></img>
             <img src={rectangle5} className='rectangle-right'></img>
           </div>
